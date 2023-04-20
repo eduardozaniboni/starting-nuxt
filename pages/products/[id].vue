69f49357c3e7f038ff3f9d447404969741752035
Eduardo Zaniboni <eduardowz.prog@gmail.com>
2023-04-19 21:51:42 -0300

    first commit

---
+++ pages/products/[id].vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>{{ product?.title }}</p>
+    <p>{{ product?.price }}</p>
+    <p>{{ product?.id }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Product {
+  title: string;
+  price: string;
+  id: string;
+}
+
+const { id } = useRoute().params;
+const uri = "https://fakestoreapi.com/products/" + id;
+
+// fetch the product
+const { data: product } = await useFetch<Product>(uri, { key: id[0] });
+
+definePageMeta({
+  layout: "products",
+});
+</script>
+
+<style scoped></style>
